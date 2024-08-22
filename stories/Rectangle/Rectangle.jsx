f0b83d6e19fd31999182c20362a2e0f8fa7859ee
LuanDan899 <truongluan280702@gmail.com>
2024-08-22 21:18:06 +0700

    add Rectangle

---
+++ stories/Rectangle/Rectangle.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './rectangle.css';
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Rectangle = ({primary, backgroundColor, size, label, ...props}) => {
+    return (
+        <div className="rectangle">
+            {label}
+        </div>
+    );
+};
+
+Rectangle.propTypes = {
+    /**
+     * Is this the principal call to action on the page?
+     */
+    primary: PropTypes.bool,
+    /**
+     * What background color to use
+     */
+    backgroundColor: PropTypes.string,
+    /**
+     * How large should the button be?
+     */
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    /**
+     * Button contents
+     */
+    label: PropTypes.string.isRequired,
+    /**
+     * Optional click handler
+     */
+    onClick: PropTypes.func,
+};
+
+Rectangle.defaultProps = {
+    backgroundColor: null,
+    primary: false,
+    size: 'medium',
+    onClick: undefined,
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
